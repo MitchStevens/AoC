031ff436b318c518b48d7a348f836bedb83f18c1
Mitch Stevens <mitchstevens95@gmail.com>
2020-11-29 12:10:55 +1100

    remove files from repo

--- 2017/Cycles.hs
+++
@@ -1,9 +0,0 @@
-CycleData = CycleData
-  { offset :: Int
-  , period :: Int
-  }
-
-analyseCycles :: (a -> b -> a) -> [a] -> Maybe CycleData
-analyseCycles f list
-
-bigFoldl' 
--- Group.hs
+++
@@ -1,7 +0,0 @@
-module Group where
-
-class Monoid g => Group g where
-  (><) :: g -> g -> g
-  
-  inv :: g -> g
-  inv = (mempty ><)
