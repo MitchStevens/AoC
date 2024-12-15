2bf43bf429460baf678853e62ce8db3531f9e09d
Mitch Stevens <mitchstevens95@gmail.com>
2024-12-16 03:41:32 +1100

    ezpush

---
+++ src/common/Braille.hs
@@ -0,0 +1,23 @@
+module Braille where
+
+import Data.Array as A
+import Point
+import Data.Bits
+import Array2D
+import Data.Int
+
+brailleUnicode :: Array Int8 Char
+brailleUnicode = A.listArray (minBound, maxBound) chars
+  where
+    chars = "⠀⠁⠂⠃⠄⠅⠆⠇⠈⠉⠊⠋⠌⠍⠎⠏⠐⠑⠒⠓⠔⠕⠖⠗⠘⠙⠚⠛⠜⠝⠞⠟⠠⠡⠢⠣⠤⠥⠦⠧⠨⠩⠪⠫⠬⠭⠮⠯⠰⠱⠲⠳⠴⠵⠶⠷⠸⠹⠺⠻⠼⠽⠾⠿⡀⡁⡂⡃⡄⡅⡆⡇⡈⡉⡊⡋⡌⡍⡎⡏⡐⡑⡒⡓⡔⡕⡖⡗⡘⡙⡚⡛⡜⡝⡞⡟⡠⡡⡢⡣⡤⡥⡦⡧⡨⡩⡪⡫⡬⡭⡮⡯⡰⡱⡲⡳⡴⡵⡶⡷⡸⡹⡺⡻⡼⡽⡾⡿⢀⢁⢂⢃⢄⢅⢆⢇⢈⢉⢊⢋⢌⢍⢎⢏⢐⢑⢒⢓⢔⢕⢖⢗⢘⢙⢚⢛⢜⢝⢞⢟⢠⢡⢢⢣⢤⢥⢦⢧⢨⢩⢪⢫⢬⢭⢮⢯⢰⢱⢲⢳⢴⢵⢶⢷⢸⢹⢺⢻⢼⢽⢾⢿⣀⣁⣂⣃⣄⣅⣆⣇⣈⣉⣊⣋⣌⣍⣎⣏⣐⣑⣒⣓⣔⣕⣖⣗⣘⣙⣚⣛⣜⣝⣞⣟⣠⣡⣢⣣⣤⣥⣦⣧⣨⣩⣪⣫⣬⣭⣮⣯⣰⣱⣲⣳⣴⣵⣶⣷⣸⣹⣺⣻⣼⣽⣾⣿"
+
+toBraille :: Int8 -> Char
+toBraille = (brailleUnicode !)
+
+--printArrayUnicode :: Array Point Bool -> String
+--printArrayUnicode = 
+--  where
+--    brailleAt :: Point -> Char
+--    brailleAt (x, y) = 
+--
+--[(x', y') | y' <- [y..(y+3)], x' <- [x,x+1]]
